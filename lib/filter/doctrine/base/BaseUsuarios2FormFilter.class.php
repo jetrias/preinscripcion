cd867b6e884d7da877267835a3e37610d040a7f0
jetrias <jetrias@gmail.com>
2018-08-07 21:19:05 -0400

    notas senamecf

---
+++ lib/filter/doctrine/base/BaseUsuarios2FormFilter.class.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Usuarios2 filter form base class.
+ *
+ * @package    preinscripcion
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterGeneratedTemplate.php 29570 2010-05-21 14:49:47Z Kris.Wallsmith $
+ */
+abstract class BaseUsuarios2FormFilter extends BaseFormFilterDoctrine
+{
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'identificacion' => new sfWidgetFormFilterInput(),
+      'usuario'        => new sfWidgetFormFilterInput(),
+      'clave'          => new sfWidgetFormFilterInput(),
+    ));
+
+    $this->setValidators(array(
+      'identificacion' => new sfValidatorPass(array('required' => false)),
+      'usuario'        => new sfValidatorPass(array('required' => false)),
+      'clave'          => new sfValidatorPass(array('required' => false)),
+    ));
+
+    $this->widgetSchema->setNameFormat('usuarios2_filters[%s]');
+
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+
+    $this->setupInheritance();
+
+    parent::setup();
+  }
+
+  public function getModelName()
+  {
+    return 'Usuarios2';
+  }
+
+  public function getFields()
+  {
+    return array(
+      'id'             => 'Number',
+      'identificacion' => 'Text',
+      'usuario'        => 'Text',
+      'clave'          => 'Text',
+    );
+  }
+}
---
+++ web/js/menu.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+$(".testnav").addClass("ui-menu ui-widget ui-widget-content ui-corner-all");
+$(".testnav li").addClass("ui-menu-item");
+}); // End of document function
